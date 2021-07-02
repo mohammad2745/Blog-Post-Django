65d5c7cb6ad9bd6b9b83abda9682f28e4c8fef14
Mohammad Bin Sultan <mohammadfcc2745@gmail.com>
2021-07-03 05:12:14 +0600

    Templates Added

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-urlpatterns = [
+from blog.views import BlogListView, BlogDetailView
 
+urlpatterns = [
+    path('', BlogListView.as_view(), name='home'),
+    path('post/<int:pk>/', BlogDetailView.as_view(), name='post_detail'),
 ]
